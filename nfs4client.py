7dcef2565b5744175dca55cd4e7a71cdc13ca3ae
Peter Åstrand <astrand@lysator.liu.se>
2001-09-26 16:14:53 +0000

    added nfs4client.py

---
+++ nfs4client.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+
+# nfs4client.py - NFS4 client. 
+#
+# Copyright (C) 2001  Peter Åstrand <peter@cendio.se>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License. 
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from nfs4constants import *
+from nfs4types import *
+import nfs4lib
+import readline
+import cmd
+import sys
+
+
+class CLI(cmd.Cmd):
+    def __init__(self):
+        # FIXME: show current directory. 
+        self.prompt = "nfs4: >"
+        # FIXME
+        #self.intro = ""
+        #self.doc_header = ""
+        #self.misc_header
+        #self.undoc_header
+
+    def do_EOF(self, line):
+        print
+        sys.exit(0)
+    
+    def do_cd(self, line):
+        print "cd is not implemented yet"
+
+    def help_foo(self):
+        print "Change current directory"
+
+    def help_overview(self):
+        # FIXME
+        print "No info yet."
+
+    def do_shell(self, line):
+        # FIXME
+        print "Not implemented yet. "
+
+    def emptyline(self):
+        pass
+
+    def default(self, line):
+        print "Unknown command: %s" % line
+
+
+
+
+if __name__ == "__main__":
+
+    c = CLI()
+    c.cmdloop()
+
+
+    
+##     import sys
+##     if len(sys.argv) < 3:
+##         print "Usage: %s <protocol> <host>" % sys.argv[0]
+##         sys.exit(1)
+    
+##     proto = sys.argv[1]
+##     host = sys.argv[2]
+##     if proto == "tcp":
+##         ncl = nfs4lib.TCPNFS4Client(host)
+##     elif proto == "udp":
+##         ncl = nfs4lib.UDPNFS4Client(host)
+##     else:
+##         raise RuntimeError, "Wrong protocol"
+
+##     # PUTROOT & GETFH
+##     putrootfhoperation = nfs_argop4(ncl, argop=OP_PUTROOTFH)
+##     getfhoperation = nfs_argop4(ncl, argop=OP_GETFH)
+    
+##     res =  ncl.compound([putrootfhoperation, getfhoperation])
+
+##     fh = res.resarray[1].opgetfh.resok4.object
+##     print "fh is", repr(fh)
+
+
+
+
