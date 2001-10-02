34d5c9109b89b72a2302ec22db070922e4138a83
Peter Åstrand <astrand@lysator.liu.se>
2001-10-02 15:46:32 +0000

    Added TODO. Added state variables to PartialNFS4Client. Convenience methods: init_connection and fetch_root. Class NFS4OpenFile: file-like object.

--- nfs4lib.py
+++ nfs4lib.py
@@ -17,6 +17,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+# TODO: Exceptions.
+# Remove self.rootfh?
 
 NFS_PROGRAM = 100003
 NFS_VERSION = 4
@@ -33,6 +35,16 @@ import os
 import pwd
 
 class PartialNFS4Client:
+    def __init__(self):
+        # Client state variables
+        self.clientid = None
+        self.verifier = None
+        # Current directory. A string, like /doc/foo. 
+        self.cwd = "/"
+        # Root directory
+        self.rootfh = None
+
+    
     def addpackers(self):
         self.packer = nfs4packer.NFS4Packer()
         self.unpacker = nfs4packer.NFS4Unpacker('')
@@ -62,7 +74,7 @@ class PartialNFS4Client:
         return socket.gethostname() + str(os.getpid())
 
     #
-    # Operations
+    # Operations. Creates complete operations. Does not send them to the server. 
     #
     def access(self):
         # FIXME
@@ -70,6 +82,8 @@ class PartialNFS4Client:
 
     def close(self):
         # FIXME
+        args = CLOSE4args(self, seqid=, stateid)
+        return nfs_argop4(self, argop=OP_CLOSE, opputfh=args)
         raise NotImplementedError()
 
     def commit(self):
@@ -132,6 +146,9 @@ class PartialNFS4Client:
 
         if not owner:
             owner = pwd.getpwuid(os.getuid())[0]
+
+        if not clientid:
+            clientid = self.clientid
 
         openhow = openflag4(self, opentype=opentype, how=how)
         owner = nfs_lockowner4(self, clientid=clientid, owner=owner)
@@ -250,7 +267,37 @@ class PartialNFS4Client:
     def cb_recall(self):
         # FIXME
         raise NotImplementedError()
+    
+    #
+    # NFS convenience methods
+    #
+    def init_connection(self):
+        # SETCLIENTID
+        op = self.setclientid()
+        res =  self.compound([op])
+        if res.status:
+            raise "SETCLIENTID failed with status", res.status
+        
+        self.clientid = res.resarray[0].arm.resok4.clientid
+        setclientid_confirm = res.resarray[0].arm.resok4.setclientid_confirm
 
+        # SETCLIENTID_CONFIRM
+        op = self.setclientid_confirm(setclientid_confirm)
+        res =  self.compound([op])
+        if res.status:
+            raise "SETCLIENTID_CONFIRM failed with status", res.status
+
+        # Fetch root filehandle.
+        self.fetch_root()
+
+    def fetch_root(self):
+        putrootfhoperation = self.putrootfh()
+        getfhoperation = self.getfh()
+        res =  self.compound([putrootfhoperation, getfhoperation])
+        if res.status:
+            raise "failed with status", res.status
+
+        self.rootfh = res.resarray[1].arm.arm.object
     
 
 class UDPNFS4Client(PartialNFS4Client, rpc.RawUDPClient):
@@ -259,6 +306,7 @@ class UDPNFS4Client(PartialNFS4Client, rpc.RawUDPClient):
 
     def mkcred(self):
 	if self.cred == None:
+            # FIXME. 
 	    self.cred = (rpc.AUTH_UNIX, rpc.make_auth_unix(1, "maggie.lkpg.cendio.se", 0, 0, [0, 1, 2, 3, 4, 6, 10]))
 	return self.cred
 
@@ -271,7 +319,142 @@ class UDPNFS4Client(PartialNFS4Client, rpc.RawUDPClient):
 class TCPNFS4Client(PartialNFS4Client, rpc.RawTCPClient):
     def __init__(self, host):
         rpc.RawTCPClient.__init__(self, host, NFS_PROGRAM, NFS_VERSION, NFS_PORT)
+        PartialNFS4Client.__init__(self)
 
+
+
+        
+
+class NFS4OpenFile:
+    """Emulates a Python file object.
+    """
+    # TODO: Implement buffering. 
+    def __init__(self, ncl):
+        self.ncl = ncl
+        self.__set_priv("closed", 1)
+        self.__set_priv("mode", "")
+        self.__set_priv("name", "")
+        self.softspace = 0
+        self.pos = 0
+        # NFS4 file handle. 
+        self.fh = None
+
+    def __setattr__(self, name, val):
+        if name in ["closed", "mode", "name"]:
+            raise TypeError("read only attribute")
+        else:
+            self.__set_priv(name, val)
+
+    def __set_priv(self, name, val):
+        self.__dict__[name] = val
+
+        # FIXME: bufsize from system default. 
+    def open(self, filename, mode="r", bufsize=1024):
+        if filename[0] != os.sep:
+            # Only absolute file names now. 
+            raise NotImplementedError()
+        else:
+            # Remove slash.
+            filename = filename[1:]
+
+        components = filename.split(os.sep)
+
+        putrootfhoperation = self.ncl.putrootfh()
+        op = self.ncl.open(file=components)
+        getfhoperation = self.ncl.getfh()
+        res =  self.ncl.compound([putrootfhoperation, op, getfhoperation])
+        # FIXME: Intelligent error handling: In this app: Print error message.
+        # In nfs4lib: Raise exception. 
+        if res.status:
+            raise " failed with status", res.status
+            
+        self.fh = res.resarray[2].arm.arm.object
+        
+
+    def close(self):
+        if not self.closed:
+            self.__set_priv("closed", 1)
+            # FIXME: NFS close. 
+            raise NotImplementedError()
+
+    def flush(self):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        raise NotImplementedError()
+
+    # isatty() should not be implemented.
+
+    # fileno() should not be implemented.
+
+    def read(self, size=None):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        # FIXME
+        pass
+
+    def readline(self, size=None):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        # FIXME
+        pass
+
+    def readlines(self, sizehint=None):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        # FIXME
+        pass
+
+    def xreadlines(self):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        import xreadlines
+        return xreadlines.xreadlines(self)
+
+    def seek(self, offset, whence=0):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        if whence == 0:
+            # absolute file positioning)
+            newpos = offset
+        elif whence == 1:
+            # seek relative to the current position
+            newpos = self.pos + offset
+        elif whence == 2:
+            # seek relative to the file's end
+            #offset += self.len
+            # FIXME: Fetch len via NFS. 
+            raise NotImplementedError()
+        else:
+            raise IOError("[Errno 22] Invalid argument")
+        self.pos = max(0, newpos)
+
+    def tell(self):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        return self.pos
+
+    def truncate(self, size=None):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        if not size:
+            size = self.pos
+        # FIXME
+        raise NotImplementedError()
+        
+    def write(self, str):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        # FIXME
+        raise NotImplementedError()
+        
+
+    def writelines(self, list):
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        # (writelines() does not add line separators)
+        # FIXME
+        raise NotImplementedError()
+        
 
 
 if __name__ == "__main__":
