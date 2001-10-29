f5872556e79d98ecddb59efbc1096b251cc4249d
Peter Åstrand <astrand@lysator.liu.se>
2001-10-29 09:34:47 +0000

    License header and description.

--- epinfo2sxw.py
+++ epinfo2sxw.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python2
 
-# http://xml.openoffice.org/
+# epinfo2sxw
+#
+# Written by Peter Åstrand <peter@cendio.se>
+# Copyright (C) 2001 Cendio Systems AB (http://www.cendio.se)
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License. 
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+# Description: Extract equivalence partitioning and testcase
+# information from docstrings (in nfs4st) and creates StarOffice
+# tables from it. For more information about the SO XML file format,
+# see http://xml.openoffice.org/. 
 
 import nfs4st
 import StringIO
