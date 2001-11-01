9a08bad633abeff86700ca994925641bf1873105
Peter Åstrand <astrand@lysator.liu.se>
2001-11-01 14:56:29 +0000

    Implemented ReadTestCase.

--- nfs4st.py
+++ nfs4st.py
@@ -1896,8 +1896,158 @@ class PutrootfhTestCase(NFSTestCase):
 
 
 class ReadTestCase(NFSTestCase):
-    # FIXME
-    pass
+    """Test READ operation
+
+    FIXME: Add attribute directory and named attribute testing. 
+
+    Equivalence partitioning:
+
+    Input Condition: current filehandle
+        Valid equivalence classes:
+            file(1)
+            named attribute(2)
+        Invalid equivalence classes:
+            dir(3)
+            special device files(4)
+            invalid filehandle(10)
+    Input Condition: offset
+        Valid equivalence classes:2
+            zero(11)
+            less than file size(12)
+            greater than or equal to file size(13)
+        Invalid equivalence classes:
+            -
+    Input Condition: count
+        Valid equivalence classes:
+            zero(14)
+            one(15)
+            greater than one(16)
+        Invalid equivalence classes:
+            -
+    Input Condition: stateid
+        Valid equivalence classes:
+            all bits zero(17)
+            all bits one(18)
+            valid stateid from open(19)
+        Invalid equivalence classes:
+            invalid stateid(20)
+    
+    """
+    def setUp(self):
+        self.connect()
+        self.putrootfhop = self.ncl.putrootfh_op()
+
+    #
+    # Testcases covering valid equivalence classes.
+    #
+    def testSimpleRead(self):
+        """READ from regular file with stateid=zeros
+
+        Covered valid equivalence classes: 1, 11, 14, 17
+        """
+        path = nfs4lib.str2pathname(self.normfile)
+        lookupop = self.ncl.lookup_op(path)
+        
+        readop = self.ncl.read(offset=0, count=0, stateid=0)
+        res = self.do_compound([self.putrootfhop, lookupop, readop])
+        self.assert_OK(res)
+
+    def testReadAttr(self):
+        """READ from named attribute
+
+        Covered valid equivalence classes: 2, 12, 16, 18
+        """
+        # FIXME: Implement rest of testcase.
+        self.info_message("(TEST NOT IMPLEMENTED)")
+
+    def testStateidOne(self):
+        """READ with offset=2, count=1, stateid=ones
+
+        Covered valid equivalence classes: 1, 12, 15, 18
+        """
+        path = nfs4lib.str2pathname(self.normfile)
+        lookupop = self.ncl.lookup_op(path)
+        
+        readop = self.ncl.read(offset=2, count=1, stateid=0xffffffffffffffffL)
+        res = self.do_compound([self.putrootfhop, lookupop, readop])
+        self.assert_OK(res)
+
+    def testWithOpen(self):
+        """READ with offset>size, count=5, stateid from OPEN
+
+        Covered valid equivalence classes: 1, 13, 16, 19
+        """
+        # OPEN
+        path = nfs4lib.str2pathname(self.normfile)
+        openop = self.ncl.open(file=path)
+        getfhop = self.ncl.getfh_op()
+        res = self.do_compound([self.putrootfhop, openop, getfhop])
+        self.assert_OK(res)
+        stateid = res.resarray[1].arm.arm.stateid
+        fh = res.resarray[2].arm.arm.object
+        
+        # README is 36 bytes. Lets use 1000 as offset.
+        putfhop = self.ncl.putfh_op(fh)
+        readop = self.ncl.read(offset=1000, count=5, stateid=stateid)
+
+        res = self.do_compound([putfhop, readop])
+        self.assert_OK(res)
+
+    #
+    # Testcases covering invalid equivalence classes.
+    #
+    def testDirFh(self):
+        """READ with (cfh)=directory should return NFS4ERR_ISDIR
+
+        Covered invalid equivalence classes: 3
+        """
+        path = nfs4lib.str2pathname(self.dirfile)
+        lookupop = self.ncl.lookup_op(path)
+        readop = self.ncl.read()
+
+        res = self.do_compound([self.putrootfhop, lookupop, readop])
+        self.assert_status(res, [NFS4ERR_ISDIR])
+
+    def testSpecials(self):
+        """READ with (cfh)=device files should return NFS4ERR_INVAL
+
+        Covered invalid equivalence classes: 4
+        """
+        for name in [self.blockfile,
+                     self.charfile,
+                     self.linkfile,
+                     self.socketfile,
+                     self.fifofile]:
+            path = nfs4lib.str2pathname(name)
+            lookupop = self.ncl.lookup_op(path)
+            readop = self.ncl.read()
+
+            res = self.do_compound([self.putrootfhop, lookupop, readop])
+
+            if res.status != NFS4ERR_INVAL:
+                self.info_message("READ on %s dit not return NFS4ERR_INVAL" % name)
+            
+            self.assert_status(res, [NFS4ERR_INVAL])
+
+    def testNoFh(self):
+        """READ without (cfh) should return NFS4ERR_NOFILEHANDLE
+
+        Covered invalid equivalence classes: 10
+        """
+        readop = self.ncl.read()
+        res = self.do_compound([readop])
+        self.assert_status(res, [NFS4ERR_NOFILEHANDLE])
+
+    def testInvalidStateid(self):
+        """READ with a (guessed) invalid stateid should return NFS4ERR_STALE_STATEID
+
+        Covered invalid equivalence classes: 20
+        """
+        readop = self.ncl.read(stateid=0x123456789L)
+        res = self.do_compound([readop])
+        self.assert_status(res, [NFS4ERR_STALE_STATEID])
+
+        
 
 class ReaddirTestCase(NFSTestCase):
     # FIXME
