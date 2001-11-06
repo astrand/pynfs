85fbd981c9384a7a5469a80bf1d027aa71b6f0a4
Peter Åstrand <astrand@lysator.liu.se>
2001-11-06 20:39:09 +0000

    Some parts of WriteTestCase implemented.

--- nfs4st.py
+++ nfs4st.py
@@ -1825,7 +1825,8 @@ class PutrootfhTestCase(NFSTestCase):
 class ReadTestCase(NFSTestCase):
     """Test READ operation
 
-    FIXME: Add attribute directory and named attribute testing. 
+    FIXME: Add attribute directory and named attribute testing.
+    FIXME: Try reading a locked file. 
 
     Equivalence partitioning:
 
@@ -2913,8 +2914,71 @@ class VerifyTestCase(NFSTestCase):
 
 
 class WriteTestCase(NFSTestCase):
-    # FIXME
-    pass
+    """Test WRITE operation
+
+    FIXME: Write to named attribute. 
+
+    Equivalence partitioning:
+
+    Input Condition: current filehandle
+        Valid equivalence classes:
+            file(10)
+            named attribute(11)
+        Invalid equivalence classes:
+            dir(12)
+            special device files(13)
+            invalid filehandle(14)
+    Input Condition: stateid
+        Valid equivalence classes:
+            all bits zero(20)
+            all bits one(21)
+            valid stateid from open(22)
+        Invalid equivalence classes:
+            invalid stateid(23)
+    Input Condition: offset
+        Valid equivalence classes:
+            zero(30)
+            nonzero(31)
+        Invalid equivalence classes:
+            -
+    Input Condition: stable
+        Valid equivalence classes:
+            UNSTABLE4(40)
+            DATA_SYNC4(41)
+            FILE_SYNC4(42)
+        Invalid equivalence classes:
+            invalid constant(43)
+    Input Condition: data
+        Valid equivalence classes:
+            no data(50)
+            some data(51)
+        Invalid equivalence classes:
+            -
+    """
+    #
+    # Testcases covering valid equivalence classes.
+    #
+    def testSimpleWrite(self):
+        """WRITE with stateid=zeros, no data and UNSTABLE4
+
+        Covered equivalence classes: 10, 20, 30, 40, 50
+        """
+
+    def testStateidOne(self):
+        """WRITE with stateid=ones and DATA_SYNC4
+
+        Covered equivalence classes: 10, 21, 31, 41, 51
+        """
+
+    def testWithOpen(self):
+        """WRITE with open and FILE_SYNC4
+
+        Covered equivalence classes: 10, 22, 30, 42, 51
+        """
+    #
+    # Testcases covering invalid equivalence classes.
+    #
+    
 
 
 class QuietTextTestRunner(unittest.TextTestRunner):
